ec44ba39e3a9b7e815dfc40af513dc0728294ffd
beitaqa25 <mari01beita@gmail.com>
2024-09-26 03:56:49 +0000

    finished

--- src/models.py
+++ src/models.py
@@ -7,25 +7,40 @@ from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
+# Asegúrate de que solo exista una clase User
+class User(Base):
+    __tablename__ = 'user'
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
+    lastname = Column(String(250), nullable=False)
+    email = Column(String(250), nullable=False)
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
+class Planeta(Base):
+    __tablename__ = 'planeta'
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    name = Column(String(250), nullable=False)
+    clima = Column(String(250), nullable=False)
+    terreno = Column(String(250), nullable=False)
 
-    def to_dict(self):
+class Personaje(Base):
+    __tablename__ = 'personaje'
+    id = Column(Integer, primary_key=True)
+    name_personaje = Column(String(250), nullable=False)
+    genero = Column(String(250), nullable=False)
+    especie = Column(String(250), nullable=False)
+
+class Favoritos(Base):
+    __tablename__ = 'favorito'
+    id = Column(Integer, primary_key=True)  # Añade una clave primaria
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
+    planeta_id = Column(Integer, ForeignKey('planeta.id'), nullable=True)
+    planeta = relationship(Planeta)
+    personaje_id = Column(Integer, ForeignKey('personaje.id'), nullable=True)
+    personaje = relationship(Personaje)
+    fecha_agregado = Column(String(250), nullable=False)
+    
+def to_dict(self):
         return {}
 
 ## Draw from SQLAlchemy base
